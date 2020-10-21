fd44df0a1fb578719ed0252b1d10519b5fa13ab1
fatpav <fat_pav@hotmail.com>
2020-10-21 13:37:22 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>Hello guys</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data() {
+    return {
+      characters: [],
+      selectedCharacter: null
+    };
+  },
+  mounted(){
+    fetch('https://rickandmortyapi.com/api/character')
+    .then(res => res.json())
+    .then(characters => this.characters = characters)
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
